1edd231e1f54bef33eb647a2eb0aacc2bf0e1a6e
aunikverma1 <aunikverma36@gmail.com>
2026-07-19 17:57:58 +0530

    LC-remove-duplicate-letters [Runtime: N/A; Memory: N/A]

--- monotonic-stack/remove-duplicate-letters.cpp
+++ monotonic-stack/remove-duplicate-letters.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> count(26, 0);
-        vector<bool> used(26, false);
-        string ans = "";
+        vector<int> freq(26, 0);
         for (char c : s) {
-            count[c - 'a']++;
+            freq[c - 'a'] += 1;
         }
+        vector<bool> used(26, false);
         stack<char> st;
+
         for (char c : s) {
-            count[c - 'a']--;
+            // sub from freq
+            freq[c - 'a'] -= 1;
+            // if used continue
             if (used[c - 'a']) {
                 continue;
             }
-            //if ans is not empty and c < ans[n - 1] and count[ans[n - 1]] means duplicate
-            while (!ans.empty() && c < ans.back() && count[ans.back() - 'a'] > 0) {
-                used[ans.back() - 'a'] = false;
-                ans.pop_back();
+            // if less than st.top() and present further in string
+            while (!st.empty() && c < st.top() && freq[st.top() - 'a'] > 0) {
+                used[st.top() - 'a'] = false;
+                st.pop();
             }
-            ans.push_back(c);
+            st.push(c);
             used[c - 'a'] = true;
         }
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
