993d05bd582d10ee72a49e26c4f080afb6567975
aunikverma1 <aunikverma36@gmail.com>
2026-07-20 18:42:00 +0530

    LC-shift-2d-grid [Runtime: N/A; Memory: N/A]

--- array/shift-2d-grid.cpp
+++ array/shift-2d-grid.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void shift(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        // swapping to i to i + 1
-        for (int i = 0; i < m; i++) {
-            int last = grid[i][n - 1];
-            for (int j = n - 1; j >= 1; j--) {
-                grid[i][j] = grid[i][j - 1];
-            }
-            grid[i][0] = last;
-        }
-        // now first col j to j + 1
-        int last = grid[m - 1][0];
-        for (int i = m - 1; i >= 1; i--) {
-            grid[i][0] = grid[i - 1][0];
-        }
-        grid[0][0] = last;
-        return;
-    }
-
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
         for (int i = 0; i < k; i++) {
-            shift(grid);
+            // swapping to i to i + 1
+            for (int i = 0; i < m; i++) {
+                int last = grid[i][n - 1];
+                for (int j = n - 1; j >= 1; j--) {
+                    grid[i][j] = grid[i][j - 1];
+                }
+                grid[i][0] = last;
+            }
+            // now first col j to j + 1
+            int last = grid[m - 1][0];
+            for (int i = m - 1; i >= 1; i--) {
+                grid[i][0] = grid[i - 1][0];
+            }
+            grid[0][0] = last;
         }
         return grid;
     }
