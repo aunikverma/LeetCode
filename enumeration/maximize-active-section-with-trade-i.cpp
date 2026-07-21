490faa337ae97977394c8bdca696dfef6503ac11
aunikverma1 <aunikverma36@gmail.com>
2026-07-21 22:45:36 +0530

    LC-maximize-active-section-with-trade-i [Runtime: N/A; Memory: N/A]

---
+++ enumeration/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<int> zeroBlock;
+        int ones = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') {
+                int count = 0;
+                while (s[i] == '0') {
+                    count += 1;
+                    i += 1;
+                }
+                i -= 1;
+                zeroBlock.push_back(count);
+            } else {
+                ones += 1;
+            }
+        }
+        int ans = ones;
+        for (int i = 1; i < zeroBlock.size(); i++) {
+            ans = max(ans, ones + zeroBlock[i - 1] + zeroBlock[i]);
+        }
+        return ans;
+    }
+};
