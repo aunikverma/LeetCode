1fa484f89390a0f3bd7f7009421052622cbaba35
aunikverma1 <aunikverma36@gmail.com>
2026-07-22 06:24:25 +0530

    LC-count-square-submatrices-with-all-ones [Runtime: N/A; Memory: N/A]

--- array/count-square-submatrices-with-all-ones.cpp
+++ array/count-square-submatrices-with-all-ones.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int m, n;
-    int dp[301][301];
-
-    int solve(int i, int j, vector<vector<int>>& matrix) {
-        if (i >= m || j >= n) {
-            return 0;
+    int solveTab(vector<vector<int>>& matrix) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int ans = 0;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == 1) {
+                    if (i == m - 1 || j == n - 1) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + min(dp[i + 1][j],
+                                           min(dp[i][j + 1], dp[i + 1][j + 1]));
+                    }
+                    ans += dp[i][j];
+                }
+            }
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        dp[i][j] = 0;
-        int right = solve(i, j + 1, matrix);
-        int down = solve(i + 1, j, matrix);
-        int dig = solve(i + 1, j + 1, matrix);
-
-        if (matrix[i][j] == 1) {
-            dp[i][j] = 1 + min(right, min(down, dig));
-        }
-        return dp[i][j];
+        return ans;
     }
 
     int countSquares(vector<vector<int>>& matrix) {
         m = matrix.size();
         n = matrix[0].size();
-
-        memset(dp, -1, sizeof(dp));
-
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    ans += solve(i, j, matrix);
-                }
-            }
-        }
-        return ans;
+        return solveTab(matrix);
     }
 };
