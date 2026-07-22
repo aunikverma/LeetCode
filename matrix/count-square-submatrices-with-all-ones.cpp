6014fc39672e3c4d0bafae1362730b2f25ddb76b
aunikverma1 <aunikverma36@gmail.com>
2026-07-22 06:29:10 +0530

    LC-count-square-submatrices-with-all-ones [Runtime: N/A; Memory: N/A]

--- matrix/count-square-submatrices-with-all-ones.cpp
+++ matrix/count-square-submatrices-with-all-ones.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int m, n;
-    int solveTab(vector<vector<int>>& matrix) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+    int solveTabSpace(vector<vector<int>>& matrix) {
+        vector<int> curr(n, 0);
+        vector<int> next(n, 0);
+
         int ans = 0;
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 if (matrix[i][j] == 1) {
                     if (i == m - 1 || j == n - 1) {
-                        dp[i][j] = 1;
+                        curr[j] = 1;
                     } else {
-                        dp[i][j] = 1 + min(dp[i + 1][j],
-                                           min(dp[i][j + 1], dp[i + 1][j + 1]));
+                        curr[j] =
+                            1 + min(next[j], min(curr[j + 1], next[j + 1]));
                     }
-                    ans += dp[i][j];
+                    ans += curr[j];
+                } else {
+                    curr[j] = 0;
                 }
             }
+            next = curr;
         }
         return ans;
     }
@@ -23,6 +28,6 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         m = matrix.size();
         n = matrix[0].size();
-        return solveTab(matrix);
+        return solveTabSpace(matrix);
     }
 };
