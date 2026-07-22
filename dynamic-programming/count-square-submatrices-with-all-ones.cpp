d4954145dd398b2812bd67ea3275294b8be2a84b
aunikverma1 <aunikverma36@gmail.com>
2026-07-22 06:03:30 +0530

    LC-count-square-submatrices-with-all-ones [Runtime: N/A; Memory: N/A]

--- dynamic-programming/count-square-submatrices-with-all-ones.cpp
+++ dynamic-programming/count-square-submatrices-with-all-ones.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int m, n;
+    int dp[301][301];
 
-    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+    int solve(int i, int j, vector<vector<int>>& matrix) {
         if (i >= m || j >= n) {
             return 0;
         }
@@ -10,9 +11,9 @@ public:
             return dp[i][j];
         }
         dp[i][j] = 0;
-        int right = solve(i, j + 1, matrix, dp);
-        int down = solve(i + 1, j, matrix, dp);
-        int dig = solve(i + 1, j + 1, matrix, dp);
+        int right = solve(i, j + 1, matrix);
+        int down = solve(i + 1, j, matrix);
+        int dig = solve(i + 1, j + 1, matrix);
 
         if (matrix[i][j] == 1) {
             dp[i][j] = 1 + min(right, min(down, dig));
@@ -24,13 +25,13 @@ public:
         m = matrix.size();
         n = matrix[0].size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        memset(dp, -1, sizeof(dp));
 
         int ans = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 1) {
-                    ans += solve(i, j, matrix, dp);
+                    ans += solve(i, j, matrix);
                 }
             }
         }
