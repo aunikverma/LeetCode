6e1be596cd4598ae0c8c60457fa6e63b643d05f1
aunikverma1 <aunikverma36@gmail.com>
2026-07-25 11:53:20 +0530

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

---
+++ hash-table/minimum-window-substring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        // base case
+        if (s == t) {
+            return s;
+        }
+        if (t.length() > s.length()) {
+            return "";
+        }
+        // sliding window
+        unordered_map<char, int> mp;
+        for (char c : t) {
+            mp[c] += 1;
+        }
+
+        int countReqd = t.length();
+        int left = 0;
+        int windowSize = INT_MAX;
+        string ans = "";
+
+        for (int right = 0; right < s.length(); right++) {
+            // subtract in map
+            if (mp[s[right]] > 0) {
+                countReqd -= 1;
+            }
+            mp[s[right]] -= 1;
+
+            // founded the window
+            if (countReqd == 0) {
+                // trying shrinking window
+                while (countReqd <= 0) {
+                    // new window
+                    if (windowSize > (right - left + 1)) {
+                        windowSize = (right - left + 1);
+                        ans = s.substr(left, windowSize);
+                    }
+                    // update in map
+                    mp[s[left]] += 1;
+                    // that we wanted it
+                    if (mp[s[left]] > 0) {
+                        countReqd += 1;
+                    }
+                    left += 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
