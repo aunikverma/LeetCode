2b85a63ae3f31a0d5229eaaddd7d43d9a78407b1
aunikverma1 <aunikverma36@gmail.com>
2026-07-29 00:00:28 +0530

    LC-smallest-palindromic-rearrangement-i [Runtime: N/A; Memory: N/A]

--- string/smallest-palindromic-rearrangement-i.cpp
+++ string/smallest-palindromic-rearrangement-i.cpp
@@ -3,7 +3,18 @@ public:
     string smallestPalindrome(string s) {
         int n = s.length();
         // sort half part
-        sort(s.begin(), s.begin() + (n / 2));
+        vector<int> freq(26, 0);
+        for (int i = 0; i < n / 2; i++) {
+            freq[s[i] - 'a'] += 1;
+        }
+        int k = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                for (int j = 0; j < freq[i]; j++) {
+                    s[k++] = ('a' + i);
+                }
+            }
+        }
 
         // making similar
         int l = 0, r = s.length() - 1;
