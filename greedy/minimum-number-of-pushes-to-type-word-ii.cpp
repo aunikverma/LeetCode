3deb6151922aa38365fbd0d69211d1e4c06c8086
aunikverma1 <aunikverma36@gmail.com>
2026-07-31 10:45:22 +0530

    LC-minimum-number-of-pushes-to-type-word-ii [Runtime: N/A; Memory: N/A]

---
+++ greedy/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans = 0;
+        vector<int> freq(26, 0);
+        for (char c : word) {
+            if (c >= 'a' && c <= 'z') {
+                freq[c - 'a'] += 1;
+            } else {
+                ans += 1;
+            }
+        }
+        sort(freq.rbegin(), freq.rend());
+        int count = 0;
+        int push = 1;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                ans += freq[i] * push;
+                count += 1;
+            }
+            if (count % 8 == 0) {
+                count = 0;
+                push += 1;
+            }
+        }
+        return ans;
+    }
+};
