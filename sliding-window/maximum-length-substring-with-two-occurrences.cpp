adaf605857b357d9fc853d2e81b921b2b4aaa8ee
aunikverma1 <aunikverma36@gmail.com>
2026-08-14 22:48:42 +0530

    LC-maximum-length-substring-with-two-occurrences [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-length-substring-with-two-occurrences.cpp
+++ sliding-window/maximum-length-substring-with-two-occurrences.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         int length = -1;
-        unordered_map<int, int> mp;
+        vector<int> mp(26, 0);
 
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
-            mp[s[right]] += 1;
-            while (mp[s[right]] > 2) {
-                mp[s[left]] -= 1;
+            mp[s[right] - 'a'] += 1;
+            while (mp[s[right] - 'a'] > 2) {
+                mp[s[left] - 'a'] -= 1;
                 left++;
             }
             length = max(length, (right - left + 1));
