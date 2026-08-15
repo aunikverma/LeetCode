44c37b6c6f0a19eadcd9be9444a49ddbd707fce2
aunikverma1 <aunikverma36@gmail.com>
2026-08-15 11:53:52 +0530

    LC-longest-subsequence-with-non-zero-bitwise-xor [Runtime: N/A; Memory: N/A]

--- array/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ array/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    unordered_map<long long, int> dp;
-
-    int solve(int i, int val, vector<int>& nums) {
-        if (i >= nums.size()) {
-            return (val > 0 ? 0 : INT_MIN);
-        }
-        long long key = ((long long)i << 32) ^ val;
-        if (dp.count(key)) {
-            return dp[key];
-        }
-        // take nums[i]
-        int n_val = (val == -1 ? nums[i] : (val ^ nums[i]));
-        int take = 1 + solve(i + 1, n_val, nums);
-        // skip nums[i]
-        int not_take = solve(i + 1, val, nums);
-        return dp[key] = max(take, not_take);
-    }
-
     int longestSubsequence(vector<int>& nums) {
-        dp.clear();
-        return max(0, solve(0, -1, nums));
+        int n = nums.size();
+        int xor_all = 0;
+        int zeros = 0;
+        for (int i : nums) {
+            xor_all ^= i;
+            zeros += (i == 0 ? 1 : 0);
+        }
+        if (xor_all == 0 && zeros == n) {
+            return 0;
+        }
+        return (xor_all > 0 ? n : n - 1);
     }
 };
