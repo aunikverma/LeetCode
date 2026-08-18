c8f178128eda9a83ccc3e1a41c2c34c7b7b9e9cc
aunikverma1 <aunikverma36@gmail.com>
2026-08-18 10:41:24 +0530

    LC-find-the-largest-almost-missing-integer [Runtime: N/A; Memory: N/A]

--- array/find-the-largest-almost-missing-integer.cpp
+++ array/find-the-largest-almost-missing-integer.cpp
@@ -6,29 +6,29 @@ public:
         for (int i : nums) {
             mp[i] += 1;
         }
-        int ans;
-        // largest which occurs only once
-        if (k == 1) {
-            ans = -1;
+        int ans = -1;
+                  // largest which occurs only once
+                  if (k == 1) {
             for (auto& v : mp) {
                 if (v.second == 1) {
                     ans = max(ans, v.first);
                 }
             }
-        } else if (k > 1 && k < n) {
-            // either nums[0] or  nums[n - 1]
-            if (mp[nums[0]] == mp[nums[n - 1]]) {
-                ans = max(nums[0], nums[n - 1]);
-            } else if (mp[nums[0]] > mp[nums[n - 1]]) {
-                ans = nums[n - 1];
-            } else {
-                ans = nums[0];
-            }
-        } else if (k == n) {
+        }
+        else if (k == n) {
             // largest in whole nums
-            ans = -1;
             for (auto& v : mp) {
                 ans = max(ans, v.first);
+            }
+        }
+        else {
+            // either nums[0] or  nums[n - 1]
+            if (mp[nums[0]] == 1 && mp[nums[n - 1]] == 1) {
+                ans = max(nums[0], nums[n - 1]);
+            } else if (mp[nums[0]] == 1) {
+                ans = nums[0];
+            } else if (mp[nums[n - 1]] == 1) {
+                ans = nums[n - 1];
             }
         }
         return ans;
