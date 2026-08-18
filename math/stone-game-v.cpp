a24d7d0be75ee543d2efd79f54324ba035cdf9c4
aunikverma1 <aunikverma36@gmail.com>
2026-08-18 11:08:07 +0530

    LC-stone-game-v [Runtime: N/A; Memory: N/A]

--- math/stone-game-v.cpp
+++ math/stone-game-v.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
-    int dp[501][501];
-
-    int solve(int l, int r, vector<int>& prefix) {
-        if (l >= r) {
-            return 0;
-        }
-        if (dp[l][r] != -1) {
-            return dp[l][r];
-        }
-        int score = 0;
-        for (int mid = l; mid <= r - 1; mid++) {
-            int left_sum = prefix[mid] - (l - 1 >= 0 ? prefix[l - 1] : 0);
-            int right_sum = prefix[r] - (mid >= 0 ? prefix[mid] : 0);
-
-            if (left_sum > right_sum) {
-                score = max(score, right_sum + solve(mid + 1, r, prefix));
-            } else if (right_sum > left_sum) {
-                score = max(score, left_sum + solve(l, mid, prefix));
-            } else {
-                score = max(score, right_sum + solve(mid + 1, r, prefix));
-                score = max(score, left_sum + solve(l, mid, prefix));
-            }
-        }
-        return dp[l][r] = score;
-    }
-
-    int stoneGameV(vector<int>& stones) {
+    int solveTab(vector<int>& stones) {
         int n = stones.size();
         // prefix sum array
         vector<int> prefix(n, 0);
@@ -34,7 +8,33 @@ public:
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + stones[i];
         }
-        memset(dp, -1, sizeof(dp));
-        return solve(0, n - 1, prefix);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int l = n - 1; l >= 0; l--) {
+            for (int r = 0; r < n; r++) {
+                if (l >= r) {
+                    continue;
+                }
+                for (int mid = l; mid <= r - 1; mid++) {
+                    int left_sum =
+                        prefix[mid] - (l - 1 >= 0 ? prefix[l - 1] : 0);
+                    int right_sum = prefix[r] - (mid >= 0 ? prefix[mid] : 0);
+
+                    if (left_sum > right_sum) {
+                        dp[l][r] = max(dp[l][r], right_sum + dp[mid + 1][r]);
+                    } else if (right_sum > left_sum) {
+                        dp[l][r] = max(dp[l][r], left_sum + dp[l][mid]);
+                    } else {
+                        dp[l][r] = max(dp[l][r], right_sum + dp[mid + 1][r]);
+                        dp[l][r] = max(dp[l][r], left_sum + dp[l][mid]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    int stoneGameV(vector<int>& stones) { 
+        return solveTab(stones); 
     }
 };
