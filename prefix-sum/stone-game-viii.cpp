4ed0b832ac1299b114c3939017d902dcff8c6a12
aunikverma1 <aunikverma36@gmail.com>
2026-08-24 12:42:20 +0530

    LC-stone-game-viii [Runtime: N/A; Memory: N/A]

--- prefix-sum/stone-game-viii.cpp
+++ prefix-sum/stone-game-viii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int n;
-
-    int solve(int i, vector<int>& prefix, vector<int>& dp) {
-        if (i == n - 1) {
-            return prefix[i];
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int take = prefix[i] - solve(i + 1, prefix, dp);
-        int skip = solve(i + 1, prefix, dp);
-        return dp[i] = max(take, skip);
-    }
-
     int stoneGameVIII(vector<int>& stones) {
-        n = stones.size();
+        int n = stones.size();
         vector<int> prefix(n, 0);
         prefix[0] = stones[0];
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + stones[i];
         }
-        vector<int> dp(n, -1);
-        return solve(1, prefix, dp);
+        vector<int> dp(n + 1, -1);
+        dp[n - 1] = prefix[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            int take = prefix[i] - dp[i + 1];
+            int skip = dp[i + 1];
+            dp[i] = max(take, skip);
+        }
+        return dp[1];
     }
 };
