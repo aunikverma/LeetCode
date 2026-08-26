9e5d17ea6cfdb7d1be0db3bfb26bde512ab3bfb0
aunikverma1 <aunikverma36@gmail.com>
2026-08-26 14:28:48 +0530

    LC-shortest-and-lexicographically-smallest-beautiful-string [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+        int ones = 0;
+        int l = 0;
+        string ans = "";
+        for (int r = 0; r < n; r++) {
+            ones += (s[r] == '1' ? 1 : 0);
+            while (ones >= k) {
+                string s_new = s.substr(l, r - l + 1);
+                if (ans.empty() || s_new.length() < ans.length() ||
+                    (s_new.length() == ans.length() &&
+                     s_new.length() < ans.length())) {
+                    ans = s_new;
+                }
+                ones += (s[l] == '1' ? -1 : 0);
+                l += 1;
+            }
+        }
+        return ans;
+    }
+};
