9e36159fae22c9c670e521171db8d0046392cf19
aunikverma1 <aunikverma36@gmail.com>
2026-08-27 11:41:59 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- counting/lexicographically-smallest-permutation-greater-than-target.cpp
+++ counting/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
-    string lexGreaterPermutation(string s, string target) {
-        sort(s.begin(), s.end());
-        // first one
-        if (s > target) {
-            return s;
-        }
-        while (next_permutation(s.begin(), s.end())) {
-            if (s > target) {
-                return s;
+    string ans = "";
+
+    bool solve(string& curr, vector<int>& count, string& target, int i,
+               bool greater) {
+        if (i == target.length()) {
+            if (greater) {
+                ans = curr;
+                return true;
             }
+            return false;
         }
-        return "";
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (count[c - 'a'] == 0) {
+                continue;
+            }
+            if (greater == false && c < target[i]) {
+                continue;
+            }
+
+            bool isGreater = (greater || c > target[i]);
+            // push
+            curr.push_back(c);
+            count[c - 'a'] -= 1;
+
+            if (solve(curr, count, target, i + 1, isGreater)) {
+                return true;
+            }
+            // pop
+            curr.pop_back();
+            count[c - 'a'] += 1;
+        }
+        return false;
+    }
+
+    string lexGreaterPermutation(string s, string target) {
+        int n = s.length();
+        vector<int> count(26, 0);
+        for (char c : s) {
+            count[c - 'a'] += 1;
+        }
+        string curr;
+        solve(curr, count, target, 0, false);
+        return ans;
     }
 };
