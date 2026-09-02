634e71ee2c797e8baf1d3ce73200f03cf3743338
aunikverma1 <aunikverma36@gmail.com>
2026-09-02 11:51:37 +0530

    LC-construct-uniform-parity-array-i [Runtime: N/A; Memory: N/A]

--- math/construct-uniform-parity-array-i.cpp
+++ math/construct-uniform-parity-array-i.cpp
@@ -1,15 +1,4 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        int even = 0, odd = 0;
-        for (int i : nums1) {
-            if (i & 1) {
-                odd += 1;
-            } else {
-                even += 1;
-            }
-        }
-        return (odd >= 1 || odd == n || even == n);
-    }
+    bool uniformArray(vector<int>& nums1) { return 1; }
 };
