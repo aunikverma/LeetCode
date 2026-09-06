ada80422bce1f0890f3f6474fe265d679b53650e
aunikverma1 <aunikverma36@gmail.com>
2026-09-06 11:59:34 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -8,10 +8,8 @@ public:
         vector<ull> curr(n + 1, 0);
 
         // base case
-        for (int i = 0; i <= m; i++) {
-            prev[0] = 1;
-            curr[0] = 1;
-        }
+        prev[0] = curr[0] = 1;
+
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s[i - 1] == t[j - 1]) {
