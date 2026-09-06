6047c4365ef119a1fd0c26eb7c6a5176d1ced5bc
aunikverma1 <aunikverma36@gmail.com>
2026-09-06 11:58:41 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     typedef unsigned long long ull;
-    int solveTab(string& s, string& t) {
+    int solveTabSpace(string& s, string& t) {
         int m = s.length();
         int n = t.length();
-        vector<vector<ull>> dp(m + 1, vector<ull>(n + 1, 0));
+        vector<ull> prev(n + 1, 0);
+        vector<ull> curr(n + 1, 0);
+
         // base case
         for (int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
+            prev[0] = 1;
+            curr[0] = 1;
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 } else {
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return curr[n];
     }
 
-    int numDistinct(string s, string t) { return solveTab(s, t); }
+    int numDistinct(string s, string t) { return solveTabSpace(s, t); }
 };
