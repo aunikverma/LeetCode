ee368b464d5a599f64af632718e46ffa9b3ec9da
aunikverma1 <aunikverma36@gmail.com>
2026-09-06 11:55:20 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -9,9 +9,6 @@ public:
         for (int i = 0; i <= m; i++) {
             dp[i][0] = 1;
         }
-        for (int i = 1; i <= n; i++) {
-            dp[0][i] = 0;
-        }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s[i - 1] == t[j - 1]) {
