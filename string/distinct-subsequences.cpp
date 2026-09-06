8e25622e73c44cb0026c8d32b1b7e322e7b6f5a6
aunikverma1 <aunikverma36@gmail.com>
2026-09-06 11:53:53 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        // t is exhausted either s may or not
-        if (j < 0 && (i >= 0 || i < 0)) {
-            return 1;
+    int solveTab(string& s, string& t) {
+        int m = s.length();
+        int n = t.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        // base case
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
-        // s is exhausted t is not
-        if (i < 0 && j >= 0) {
-            return 0;
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 0;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-        int ans = 0;
-        // if match
-        if (s[i] == t[j]) {
-            ans += solve(i - 1, j - 1, s, t, dp);
-        }
-        ans += solve(i - 1, j, s, t, dp);
-        return dp[i][j] = ans;
+        return dp[m][n];
     }
 
-    int numDistinct(string s, string t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-        vector<vector<int>> dp(i + 1, vector<int>(j + 1, -1));
-        return solve(i, j, s, t, dp);
-    }
+    int numDistinct(string s, string t) { return solveTab(s, t); }
 };
