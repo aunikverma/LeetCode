41011d97caa0481a63afe3654ea2fc188ad20e80
aunikverma1 <aunikverma36@gmail.com>
2026-09-15 11:38:50 +0530

    LC-maximum-number-of-non-overlapping-palindrome-substrings [Runtime: N/A; Memory: N/A]

--- string/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ string/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    bool is_palindrome(string& s, int i, int j) {
-        while (i < j) {
-            if (s[i] != s[j]) {
-                return false;
+    void fill(vector<vector<bool>>& is_palindrome, string& s) {
+        // length
+        // is_palindrome s[i..j]
+        int n = s.length();
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
+                if (i == j) {
+                    // single char
+                    is_palindrome[i][j] = true;
+                } else if (i + 1 == j) {
+                    // double char
+                    is_palindrome[i][j] = (s[i] == s[j]);
+                } else {
+                    // more than 2
+                    is_palindrome[i][j] =
+                        ((s[i] == s[j]) && is_palindrome[i + 1][j - 1]);
+                }
             }
-            i++;
-            j--;
         }
-        return true;
     }
 
     int maxPalindromes(string s, int k) {
@@ -17,11 +28,15 @@ public:
         if (k == 1) {
             return n;
         }
+        vector<vector<bool>> is_palindrome(n + 1, vector<bool>(n + 1, false));
+        // filling is_palindrome
+        fill(is_palindrome, s);
+        // dp array
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (is_palindrome(s, i, j)) {
+                if (is_palindrome[i][j]) {
                     int take = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
                     int grow = dp[i][j + 1];
                     int slide = dp[i + 1][j + 1];
