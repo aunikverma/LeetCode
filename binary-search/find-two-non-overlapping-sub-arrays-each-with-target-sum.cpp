c25fd72e884277fdce9a8a0c5746136b56f51ac4
aunikverma1 <aunikverma36@gmail.com>
2026-09-18 00:47:58 +0530

    LC-find-two-non-overlapping-sub-arrays-each-with-target-sum [Runtime: N/A; Memory: N/A]

--- binary-search/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ binary-search/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -3,25 +3,26 @@ public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
         vector<int> min_len(n, INT_MAX);
-
-        int curr_sum = 0, best_min = INT_MAX;
-        int l = 0;
-
         int ans = INT_MAX;
+        int l = 0, curr_sum = 0;
+        int curr_minlen = INT_MAX;
+
         for (int r = 0; r < n; r++) {
             curr_sum += arr[r];
             // shrink
             while (l < r && curr_sum > target) {
-                curr_sum -= arr[l];
-                l++;
+                curr_sum -= arr[l++];
             }
             if (curr_sum == target) {
+                int len = r - l + 1;
+                // min before this win[l..r]
                 if (l > 0 && min_len[l - 1] != INT_MAX) {
-                    ans = min(ans, (r - l + 1) + min_len[l - 1]);
+                    ans = min(ans, len + min_len[l - 1]);
+                    curr_minlen = min(curr_minlen, len);
                 }
-                best_min = min(best_min, r - l + 1);
+                curr_minlen = min(curr_minlen, len);
             }
-            min_len[r] = best_min;
+            min_len[r] = curr_minlen;
         }
         return (ans != INT_MAX ? ans : -1);
     }
